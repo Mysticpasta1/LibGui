e5efc94c787ac3734027794a1acc753dc98affcb
Mysticpasta1 <ethana5tc@outlook.com>
2024-01-09 16:42:31 -0600

    client stuff

--- src/main/java/io/github/cottonmc/cotton/gui/impl/client/LibGuiClient.java
+++ src/main/java/io/github/cottonmc/cotton/gui/impl/client/LibGuiClient.java
@@ -4,7 +4,9 @@ import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 
 import io.github.cottonmc.cotton.gui.impl.ScreenNetworkingImpl;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -13,7 +15,8 @@ public class LibGuiClient {
 	public static final Logger logger = LogManager.getLogger();
 	public static volatile LibGuiConfig config;
 
-	public LibGuiClient() {
+	@SubscribeEvent
+	public static void init(FMLClientSetupEvent event) {
 
 		ClientPlayNetworking.registerGlobalReceiver(ScreenNetworkingImpl.SCREEN_MESSAGE_S2C, (client, networkHandler, buf, responseSender) -> {
 			ScreenNetworkingImpl.handle(client, client.player, buf);
